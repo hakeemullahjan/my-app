874c460509cbb8d2ebc89357751a2ac9b7830e76
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2024-03-12 14:52:06 +0500

    revert back

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,6 @@ cd /app
 # install dependencies
 npm install
 npm run build
-# cp -r build/* /var/www/html
-cp -R build/ /var/www/html/
+cp -r build/* /var/www/html
+# cp -R build/ /var/www/html/
 npm install pm2 -g
